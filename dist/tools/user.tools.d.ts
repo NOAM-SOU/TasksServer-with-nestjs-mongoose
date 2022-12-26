c95b397ffc6600d4984a5f2c47bc816d620a7292
NOAM-SOU <78881776+NOAM-SOU@users.noreply.github.com>
2022-12-26 23:04:44 +0200

    Delete dist directory

--- dist/tools/user.tools.d.ts
+++
@@ -1,3 +0,0 @@
-import { UserDetails } from 'src/users/interfaces/UserDetails';
-import { UserDocument } from 'src/users/schemas/user.schema';
-export declare function getUser(user: UserDocument): Promise<UserDetails>;
--- dist/users/interfaces/User.d.ts
+++
@@ -1,7 +0,0 @@
-import { TaskI } from 'src/tasks/interfaces/Task';
-export interface UserI {
-    name: string;
-    email: string;
-    password: string;
-    tasks: TaskI[];
-}
